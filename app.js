c86081d0bdfd8a51c2e16a0df0ba154d6a97e4d7
AffonsoEly <101416129+AffonsoEly@users.noreply.github.com>
2025-05-12 21:11:47 -0300

    Add CSS e JS e banco de dados

---
+++ app.js
@@ -0,0 +1,7 @@
+const db = new PouchDB('Finances');
+const form = document.getElementById('RecordForm');
+const list = document.getElementById('RecordsList');
+const TotalDaily = document.getElementById('DailySum');
+const Total = document.getElementById('TotalSum');
+
+form.addEventListener('submit', async (e) => {
